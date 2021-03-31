5b3068a1301185e9ec4d56333db3dab7976f06f0
Ian Kim <ian@ianmkim.com>
2021-03-31 16:54:12 -0400

    added some decimation code

--- terrain/test.cpp
+++ terrain/test.cpp
@@ -8,6 +8,7 @@
 #include "someTools.h"
 #include "render.h"
 #include "mountain.h"
+#include "decimation.h"
 
 using namespace GEOM_FADE25D;
 
@@ -65,18 +66,6 @@ void write(const std::string& name, const Fade_2D& dt){
     dt.showGeomview((name + ".list").c_str());
     std::cout << gRed("Writing " + name + ".obj") << std::endl;
     dt.writeObj((name + ".obj").c_str());
-}
-
-int random(int low, int high){
-    return low + (std::rand() % (high - low + 1));
-}
-
-void decimate(std::vector<Point2> *points, float percentage){
-    int amountToRemove = points->size() - int(percentage * points->size());
-    if(amountToRemove < 0) return;
-    for(int i = 0; i < amountToRemove; i++){
-        points->erase(points->begin() + random(0, points->size()));
-    }
 }
 
 int main(int argc, char** argv){
